99af27840a79ce58c58eb30475cf024d4cd73247
Alex <alexcoop@Codes-MacBook-Pro.local>
2020-10-21 10:20:28 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+
+const app = express();
+//INITIALIZE EXPRESS
+
+const routes = require('routes');
+//SET UP ROUTES TO KEEP INDEX.JS CLEAN
+
+app.use('/', routes);
+
+app.listen(process.env.PORT || 3005, () => {
+    console.log("Server is listening");
+})
